ee396c73e8d6e8ec49dfdd1bd502d3f5b8683947
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-06-08 11:33:07 -0500

    1-5

--- index.js
+++ index.js
@@ -18,20 +18,27 @@ app.use(cors());
 
 // convertir datos del body a js
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // cargar conf rutas
+const UserRoutes = require("./routes/user");
+const PublicationRoutes = require("./routes/publication");
+const FollowRoutes = require("./routes/follow")
+
+app.use("/api", UserRoutes);
+app.use("/api", PublicationRoutes);
+app.use("/api", FollowRoutes)
 
 //ruta  de prueba
-app.get("/ruta-prueba", (req, res)=>{
+app.get("/ruta-prueba", (req, res) => {
   return res.status(200).json({
-    "id":1,
-    "nombre": "Napoleon",
-    "web":"codewizardai.com"
-  })
-})
+    id: 1,
+    nombre: "Napoleon",
+    web: "codewizardai.com",
+  });
+});
 
 // poner servidor a escuchar http
-app.listen(puerto, ()=>{
-  console.log("Node.js server running on port: ", puerto)
-})
+app.listen(puerto, () => {
+  console.log("Node.js server running on port: ", puerto);
+});
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/user");
+
+// Definir rutas
+router.get("/prueba-user", UserController.pruebaUser);
+
+// exportar router
+module.exports = router;
