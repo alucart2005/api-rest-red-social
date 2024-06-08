4eb73e18afaf446bbc266a5d8237f22e9ea07fc4
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-06-08 09:04:45 -0500

    1-4

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 // importar dependencias
 const connection = require("./database/connection");
-console.log("** SOCIAL NETWORK API NODE - UP AND RUNNING **")
+const express = require("express");
+const cors = require("cors");
+
+// mensaje de bienvenida
+console.log("** SOCIAL NETWORK API NODE - UP AND RUNNING **");
+
 // conexion a bbdd
 connection();
-// crear servidor node
 
-// configurar cors
+// crear servidor node
+const app = express();
+const puerto = 3900;
+
+// configurar cors (para el uso de middleware)
+app.use(cors());
 
 // convertir datos del body a js
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 // cargar conf rutas
 
+//ruta  de prueba
+app.get("/ruta-prueba", (req, res)=>{
+  return res.status(200).json({
+    "id":1,
+    "nombre": "Napoleon",
+    "web":"codewizardai.com"
+  })
+})
+
 // poner servidor a escuchar http
+app.listen(puerto, ()=>{
+  console.log("Node.js server running on port: ", puerto)
+})
