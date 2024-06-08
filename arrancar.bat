56d7a4c05d8c3dfc21a3457c3ceb785151edc7d1
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-06-08 08:15:16 -0500

    1-2

---
+++ arrancar.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "c: && cd C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe"
+start cmd.exe /k "h: && cd H:\DV\Udemy\nodejs-mongodb\api-rest-red-social && npm start"
---
+++ index.js
@@ -0,0 +1,13 @@
+// importar dependencias
+const {connection} = require("./database/connection")
+// conexion a bbdd
+
+// crear servidor node
+
+// configurar cors
+
+// convertir datos del body a js
+
+// cargar conf rutas
+
+// poner servidor a escuchar http
