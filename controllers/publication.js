3c350679b4ab7e9b4a1046b8405dbc7acd4bfb21
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-07-09 21:26:59 -0500

    8-2

--- controllers/publication.js
+++ controllers/publication.js
@@ -8,7 +8,60 @@ const pruebaPublication = (req, res) => {
 };
 
 // Guardar publicaciones
+const save = async (req, res) => {
+  try {
+    const params = req.body;
+    if (!params.text) {
+      return res.status(400).send({
+        status: "error",
+        message: "Debes enviar el texto de la publicacion",
+      });
+    }
+
+    const newPublication = new Publication(params);
+    newPublication.user = req.user.id;
+
+    const publicationStored = await newPublication.save();
+    if (!publicationStored) {
+      return res.status(400).send({
+        status: "error",
+        message: "No se ha guardado la publicacion.",
+      });
+    }
+
+    return res.status(200).send({
+      status: "Success",
+      message: "Publicacion guardada",
+      publicationStored,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Error al guardar publicacion",
+    });
+  }
+};
+
 // Listar una publicacion en concreto
+const detail = async (req, res) => {
+  try {
+    const publicationId = req.params.id;
+    const publicationStored = await Publication.findById(publicationId);
+
+    return res.status(200).send({
+      status: "success",
+      message: "Mostrar publicacion",
+      publication: publicationStored,
+    });
+    
+  } catch (error) {
+    return res.status(404).json({
+      status: "error",
+      message: "No existe la publicacion",
+    });
+  }
+};
+
 // Eliminar publicaciones
 // Listar todas las publicaciones
 // Listar publicaciones de un usuario
@@ -18,4 +71,6 @@ const pruebaPublication = (req, res) => {
 //Exportar acciones
 module.exports = {
   pruebaPublication,
+  save,
+  detail,
 };
